7f6659c2e704bbe6f326e0b6d843cc4350cc78e4
Alexander Ponte Reynaldo <alexanderponte7@gmail.com>
2024-07-31 15:32:15 -0500

    Cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import { BaileysProvider, handleCtx } from "@bot-whatsapp/provider-baileys"
 import { createBot, createProvider, createFlow, MemoryDB } from '@bot-whatsapp/bot'
-
 import QRPortalWeb from '@bot-whatsapp/portal'
+import Cors from 'cors'
+
+const corsOptions = {
+    origin: 'https://main.d3rrj4e0k1dvra.amplifyapp.com', // Reemplaza con tu dominio
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Si necesitas enviar cookies
+};
 
 const main = async () => {
     const provider = createProvider(BaileysProvider)
-
+    provider.http?.server.use(Cors(corsOptions))
     provider.initHttpServer(3002)
     provider.http?.server.post('/send',handleCtx(async (bot,req,res)=>{
         console.log(req.body)
@@ -21,7 +28,7 @@ const main = async () => {
         provider
     })
 
-    QRPortalWeb()
+    // QRPortalWeb()
 
 }
 
